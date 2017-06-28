05fba1c9ae0dcec47475393fcf81361a8ebe55f6
Nelson Gustavo <nelsongborges@gmail.com>
2017-06-27 21:24:43 -0300

    Eject and layout changes

--- src/form-box.js
+++ src/form-box.js
@@ -6,9 +6,9 @@ class FormBox extends Component {
   render() {
     return (
       <div className="form-box">
-        <img className="form-image" src=""></img>
-        <h1>Sacadas de Coaching, carreira, comunicação e mindset.</h1>
-        <p>Descubra como o Coaching pode transformar a sua carreira.</p>
+        <img className="form-image" src="https://firebasestorage.googleapis.com/v0/b/euinfluencer.appspot.com/o/oney-foto-lp-youtube.jpg?alt=media&token=a33eac1d-c29f-49af-bb1b-b2b6577d89f9"></img>
+        <h1>Sacadas, insights e direcionamentos de mídia social para Digital Influencers.</h1>
+        <p>Influencie pessoas pelo mundo inteiro.</p>
         <FormContent />
       </div>
     );
