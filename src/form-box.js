aa2d7089bf5dd3bb75a07bf6576dbf167b28629f
Nelson Gustavo <nelsongborges@gmail.com>
2017-06-28 12:12:46 -0300

    Changing content and list

--- src/form-box.js
+++ src/form-box.js
@@ -7,8 +7,8 @@ class FormBox extends Component {
     return (
       <div className="form-box">
         <img className="form-image" src="https://firebasestorage.googleapis.com/v0/b/***REMOVED***.appspot.com/o/oney-foto-lp-youtube.jpg?alt=media&token=a33eac1d-c29f-49af-bb1b-b2b6577d89f9"></img>
-        <h1>Sacadas, insights e direcionamentos de mídia social para Digital Influencers.</h1>
-        <p>Influencie pessoas pelo mundo inteiro.</p>
+        <h1>I Workshop online Eu Influencer</h1>
+        <p>Sacadas, insights, Marketing viral, Engajamento de audiência, comportamento digital.</p>
         <FormContent />
       </div>
     );
