f075e0cb69336e153dc362d71310c1020b1d07c5
Nelson Gustavo <nelsongborges@gmail.com>
2017-06-28 12:12:46 -0300

    Changing content and list

--- src/form-content.js
+++ src/form-content.js
@@ -7,7 +7,7 @@ class FormContent extends Component {
       <div className="form-content">
         <form action="https://canaldesucesso.us13.list-manage.com/subscribe/post" method="post" className="validate" noValidate>
           <input type="hidden" name="u" value="cc49294c8f423b98bac86a0e0" />
-          <input type="hidden" name="id" value="0fa0b648fb" />
+          <input type="hidden" name="id" value="f5a5e850b4" />
           <input type="email" className="email" placeholder="Digite o seu e-mail" name="MERGE0" id="MERGE0" defaultValue="" />
           <input type="submit" value="QUERO ME INSCREVER!" name="subscribe" className="button" />
         </form>
