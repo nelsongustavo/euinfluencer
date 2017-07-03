7e1f383f2c49a41b4d102880dcca40014700c82a
Nelson Gustavo <nelsongborges@gmail.com>
2017-07-03 17:20:47 -0300

    new structure

---
+++ src/form/formContainer.js
@@ -0,0 +1,24 @@
+import React, { Component } from 'react';
+import FormContent from './form'
+import FacebookProvider, { Like } from 'react-facebook';
+import { YouTubeButton } from 'react-social-sharebuttons';
+
+class FormContainer extends Component {
+  render() {
+    return (
+      <div className="form-container">
+        <FormContent
+          imageUrl="https://firebasestorage.googleapis.com/v0/b/euinfluencer.appspot.com/o/oney-foto-lp-youtube.jpg?alt=media&token=a33eac1d-c29f-49af-bb1b-b2b6577d89f9"
+          title="I Workshop Online: Eu Influencer"
+          text="Sacadas, Insights, Marketing viral, Engajamento de audiência, Comportamento digital."
+        />
+        <FacebookProvider appId="119817438197601">
+          <Like href="https://www.facebook.com/oney.araujo/" colorScheme="dark" showFaces share />
+        </FacebookProvider>
+        <YouTubeButton channel="niimakeup"/>
+      </div>
+    );
+  }
+}
+
+export default FormContainer;
---
+++ src/form/index.js
@@ -0,0 +1,3 @@
+//index.js
+import Root from './formContainer'
+export default Root
