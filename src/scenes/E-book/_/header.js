54111c14eb16b37341092fbe218e7dc4b05ed20d
Nelson Gustavo <nelsongborges@gmail.com>
2017-07-29 11:51:01 -0300

    Adicionando pagina do e-book

---
+++ src/scenes/E-book/_/header.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import { Navbar } from 'react-bootstrap';
+
+export default () => {
+  return (
+    <Navbar inverse className="header">
+      <a href="/">Euinfluencer.com</a>
+    </Navbar>
+  );
+}
