f300ee5779fc91d907c80e3c5bd1fb7ac8dec06c
Nelson Gustavo <nelsongborges@gmail.com>
2017-07-31 09:59:41 -0300

    Ajustes no roteamento do firebase

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter, Route } from 'react-router-dom';
 import Ebook from './scenes/E-book';
+import App from './scenes/App';
 import registerServiceWorker from './registerServiceWorker';
 import ga from 'react-ga';
 
@@ -11,6 +12,7 @@ ReactDOM.render(
   <BrowserRouter>
     <div>
       <Route exact path="/" component={Ebook} />
+      <Route exact path="/app" component={App} />
     </div>
   </BrowserRouter>,
   document.getElementById('root'));
