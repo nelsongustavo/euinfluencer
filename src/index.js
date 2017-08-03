5ad009bee5acfeb50e04f28b2d707acc95edf6e4
Nelson Gustavo <nelsongborges@gmail.com>
2017-08-03 18:03:23 -0300

    Refactoring

--- src/index.js
+++ src/index.js
@@ -1,18 +1,22 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { BrowserRouter, Route } from 'react-router-dom';
-import Ebook from './scenes/E-book';
-import App from './scenes/App';
-import registerServiceWorker from './registerServiceWorker';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import ga from 'react-ga';
 
+import Ebook from './scenes/ebook';
+import App from './scenes/app';
+import registerServiceWorker from './registerServiceWorker';
+
 ga.initialize('UA-102606035-1');
+ga.pageview(window.location.pathname);
 
 ReactDOM.render(
   <BrowserRouter>
     <div>
-      <Route exact path="/" component={Ebook} />
-      <Route exact path="/app" component={App} />
+      <Switch>
+        <Route exact path="/app" component={App} />
+        <Route exact path="/" component={Ebook} />
+      </Switch>
     </div>
   </BrowserRouter>,
   document.getElementById('root'));
--- src/scenes/ebook/index.js
+++ src/scenes/ebook/index.js
@@ -1,3 +1,3 @@
 //index.js
-import Root from './E-book'
+import Root from './ebook'
 export default Root
