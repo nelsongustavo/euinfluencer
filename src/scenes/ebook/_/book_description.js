388b37e29831aa5c996b40791925b7ec98e6ec75
Nelson Gustavo <nelsongborges@gmail.com>
2017-08-04 11:48:28 -0300

    Adicionando klickmail

--- src/scenes/ebook/_/book_description.js
+++ src/scenes/ebook/_/book_description.js
@@ -1,41 +1,85 @@
-import React from 'react';
-import { Button} from 'react-bootstrap';
+import React, { Component } from 'react';
+import { Button, Modal, FormGroup, ControlLabel, FormControl } from 'react-bootstrap';
 import ga from 'react-ga';
 import './main.css';
 
-export default () => {
-  return (
-    <div className="book-description">
-      <h1>10 PASSOS PARA SE TORNAR UM INFLUENCIADOR DIGITAL</h1>
-      <section>
-        <p>Por Oney Araújo</p>
-        <p>
-          Nesse e-book eu concentrei os meus 7 anos de trabalho com mídias sociais em 10
-          passos simples para você entrar e compreender de uma vez por todas o mundo dos
-          influenciadores digitais.
-        </p>
-        <p>
-          Para ser sincero, eu gostaria muito de ter acesso a algo semelhante quando comecei.
-          O diferencial desse material é o impacto que ele vai gerar na sua forma de produzir conteúdo,
-          agir e pensar daqui pra frente.
-        </p>
-        <p>
-          Então aproveite, leia e releia estas instruções e coloque cada vírgula em prática,
-          em pouco tempo você verá o seu conteúdo decolando nesse mundão digital.
-        </p>
-      </section>
-      <form action="https://***REMOVED***.us13.list-manage.com/subscribe/post" method="post" className="validate" noValidate>
-        <input type="hidden" name="u" value="cc49294c8f423b98bac86a0e0" />
-        <input type="hidden" name="id" value="d53147f679" />
-        <Button type="submit" bsStyle="danger" bsSize="large" onClick={addEvent}>FAZER DOWNLOAD DO E-BOOK GRÁTIS</Button>
-      </form>
-    </div>
-  );
-}
+export default class BookDescription extends Component {
 
-function addEvent() {
-  ga.event({
-    category: 'User',
-    action: 'CLICKED DOWNLOAD BUTTON'
-  });
+  constructor() {
+    super();
+
+    this.state = {
+      modalIsOpen: false
+    };
+
+    this.openModal = this.openModal.bind(this);
+    this.closeModal = this.closeModal.bind(this);
+  }
+
+  openModal() {
+    this.setState({modalIsOpen: true});
+  }
+
+  closeModal() {
+    this.setState({modalIsOpen: false});
+  }
+
+  addEvent() {
+    ga.event({
+      category: 'User',
+      action: 'CLICKED DOWNLOAD BUTTON'
+    });
+  }
+
+  render() {
+    return (
+      <div className="book-description">
+        <h1>10 PASSOS PARA SE TORNAR UM INFLUENCIADOR DIGITAL</h1>
+        <section>
+          <p>Por Oney Araújo</p>
+          <p>
+            Nesse e-book eu concentrei os meus 7 anos de trabalho com mídias sociais em 10
+            passos simples para você entrar e compreender de uma vez por todas o mundo dos
+            influenciadores digitais.
+          </p>
+          <p>
+            Para ser sincero, eu gostaria muito de ter acesso a algo semelhante quando comecei.
+            O diferencial desse material é o impacto que ele vai gerar na sua forma de produzir conteúdo,
+            agir e pensar daqui pra frente.
+          </p>
+          <p>
+            Então aproveite, leia e releia estas instruções e coloque cada vírgula em prática,
+            em pouco tempo você verá o seu conteúdo decolando nesse mundão digital.
+          </p>
+        </section>
+        <Button type="submit" bsStyle="danger" bsSize="large" onClick={this.openModal}>FAZER DOWNLOAD DO E-BOOK GRÁTIS</Button>
+
+        <Modal show={this.state.modalIsOpen} onHide={this.closeModal}>
+         <Modal.Header closeButton>
+           <Modal.Title>Siga os passos abaixo corretamente para você se inscrever e fazer o download.</Modal.Title>
+         </Modal.Header>
+         <Modal.Body>
+           <form id="ktv2-form-44514" className="form" accept-charset="UTF-8" method="post" action="https://www.klickmail.com.br/api/subscriber/signin.html" >
+            <input type="hidden" id="FormField_ApiKey" name="apikey" value="11btzsjkz8zec7d" />
+            <FormGroup controlId="formInlineEmail">
+              <span className="text-step">1° PASSO</span>
+              <ControlLabel>Preencha os campos abaixo</ControlLabel>
+              <FormControl type="email" placeholder="Entre com o seu melhor E-mail" id="FormField_EmailAddress" name="email" bsSize="large"/>
+            </FormGroup>
+            <FormGroup id="step-2">
+              <span className="text-step2">2° PASSO</span>
+              <label>Clique e confirme sua inscrição</label>
+              <Button type="submit" bsStyle="danger" bsSize="large">
+                BAIXE GRÁTIS!
+              </Button>
+            </FormGroup>
+          </form>
+         </Modal.Body>
+         <Modal.Footer>
+           <p>Suas informações estão seguras.</p>
+         </Modal.Footer>
+       </Modal>
+      </div>
+    );
+  }
 }
--- src/scenes/ebook/_/header.js
+++ src/scenes/ebook/_/header.js
@@ -4,7 +4,7 @@ import { Navbar } from 'react-bootstrap';
 export default () => {
   return (
     <Navbar inverse className="header">
-      <a href="/">Euinfluencer.com</a>
+      <a href="/">Euinfluencer<span>.com</span></a>
     </Navbar>
   );
 }
