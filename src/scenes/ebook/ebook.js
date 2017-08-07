39d2aea304a4580024183c9c7a14b5222b4e10d8
Nelson Gustavo <nelsongborges@gmail.com>
2017-08-07 12:38:54 -0300

    refactoring and thanks page

--- src/scenes/ebook/ebook.js
+++ src/scenes/ebook/ebook.js
@@ -1,31 +1,31 @@
 import React from 'react';
 import { Grid, Row, Col, Image } from 'react-bootstrap';
-import Header from './_/header';
-import Footer from './_/footer';
+import Layout from '../../shared';
 import BookDescription from './_/book_description'
 import AuthorDescription from './_/author_description'
+import './ebook.css';
 
 export default () => {
   return (
     <div>
-      <Header />
-      <section>
-        <Grid>
-          <Row className="show-grid">
-            <Col md={4} mdOffset={1}><Image className="center-block" src="https://firebasestorage.googleapis.com/v0/b/***REMOVED***.appspot.com/o/ebook%20(1).png?alt=media&token=25556824-8830-4902-a6bb-506b4e561ed1" rounded /></Col>
-            <Col md={7} ><BookDescription /></Col>
-          </Row>
-        </Grid>
-      </section>
-      <section className="about">
-        <Grid>
-          <Row>
-            <Col md={3} mdOffset={2}><Image className="center-block" src="https://firebasestorage.googleapis.com/v0/b/***REMOVED***.appspot.com/o/oney_redondo.png?alt=media&token=98e4f209-c3ab-4a7c-96c4-35029a6751ab" rounded /></Col>
-            <Col md={7} ><AuthorDescription /></Col>
-          </Row>
-        </Grid>
-      </section>
-      <Footer />
+      <Layout>
+        <section>
+          <Grid>
+            <Row className="show-grid">
+              <Col md={4} mdOffset={1}><Image className="center-block" src="https://firebasestorage.googleapis.com/v0/b/***REMOVED***.appspot.com/o/ebook%20(1).png?alt=media&token=25556824-8830-4902-a6bb-506b4e561ed1" rounded /></Col>
+              <Col md={7} ><BookDescription /></Col>
+            </Row>
+          </Grid>
+        </section>
+        <section className="about">
+          <Grid>
+            <Row>
+              <Col md={3} mdOffset={2}><Image className="center-block" src="https://firebasestorage.googleapis.com/v0/b/***REMOVED***.appspot.com/o/oney_redondo.png?alt=media&token=98e4f209-c3ab-4a7c-96c4-35029a6751ab" rounded /></Col>
+              <Col md={7} ><AuthorDescription /></Col>
+            </Row>
+          </Grid>
+        </section>
+      </Layout>
     </div>
   );
 }
--- src/scenes/thanks/thanks.js
+++ src/scenes/thanks/thanks.js
@@ -1,18 +1,22 @@
 import React from 'react';
+import Layout from '../../shared';
 import { Grid } from 'react-bootstrap';
+import './thanks.css';
 
 export default () => {
   return (
-    <div>
-      <section>
-        <Grid>
-          <h1>Obrigado por se inscrever!</h1>
+    <div className="thanks">
+      <Layout>
         <section>
-          <p>Em breve você vai receber um e-mail de confirmação, confirme sua inscrição e receba o e-book.</p>
-          <p>Não se esqueça de olhar a caixa de spam o e-mail pode ter ido parar lá por engano.</p>
+          <Grid>
+            <h1>Obrigado por se inscrever!</h1>
+          <section>
+            <p>Em breve você vai receber um e-mail de confirmação, confirme sua inscrição e receba o e-book.</p>
+            <p>Não se esqueça de olhar a caixa de spam o e-mail pode ter ido parar lá por engano.</p>
+          </section>
+          </Grid>
         </section>
-        </Grid>
-      </section>
+      </Layout>
     </div>
   );
 }
