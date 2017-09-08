abcdfa53ed885c181f47d3d4b72123d60eef7f17
Nelson Gustavo <nelsongborges@gmail.com>
2017-09-08 11:56:22 -0300

    week component working

--- src/shared/week_layout/week_layout.js
+++ src/shared/week_layout/week_layout.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import Header from './_/header';
-import Menu from './_/menu';
 import Footer from './_/footer';
 import './week_layout.css';
 
@@ -8,7 +7,6 @@ export default (props) => {
   return (
     <div>
       <Header />
-      <Menu />
         {props.children}
       <Footer />
     </div>
