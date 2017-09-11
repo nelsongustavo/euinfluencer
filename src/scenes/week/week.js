aec474e393caa45e96c897e01c746ab0a9e8517e
Nelson Gustavo <nelsongborges@gmail.com>
2017-09-11 10:28:25 -0300

    toggle menu mobile

--- src/scenes/week/week.js
+++ src/scenes/week/week.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import Video from './_/video';
 import Layout from '../../shared/week_layout';
+import $ from "jquery";
 import { Grid, Nav, NavItem, Glyphicon } from 'react-bootstrap';
 import FacebookProvider, { Comments } from 'react-facebook';
 import './week.css';
@@ -20,17 +21,30 @@ class WorkShop extends Component {
     this.setState({videoId: selectedKey})
   }
 
+  componentDidMount() {
+    const sideslider = $('[data-toggle=collapse-side]');
+    const sel = sideslider.attr('data-target');
+    sideslider.click(function(){
+      $(sel).toggleClass('in');
+    });
+  }
+
   render() {
     return (
       <div className="week">
         <Layout>
           <Grid>
-            <Nav bsStyle="pills" justified activeKey={this.state.videoId} onSelect={this.handleSelect}>
-              <NavItem className="first" eventKey={"nOQipNj-Nv8"} title="aula 1">Aula 01 (11/09)</NavItem>
-              <NavItem eventKey={2} title="aula 2" disabled><Glyphicon glyph="lock" /> Aula 02 (12/09)</NavItem>
-              <NavItem eventKey={3} title="aula 3" disabled><Glyphicon glyph="lock" /> Aula 03 (13/09)</NavItem>
-              <NavItem eventKey={4} title="aula 4" disabled><Glyphicon glyph="lock" /> Aula 04 (14/09)</NavItem>
-            </Nav>
+            <div className="navbar-header">
+              <button data-toggle="collapse-side" data-target=".side-collapse" data-target-2=".side-collapse-container" type="button" className="navbar-toggle pull-left"><span className="icon-bar"></span><span className="icon-bar"></span><span className="icon-bar"></span></button>
+            </div>
+            <div className="navbar-inverse side-collapse in">
+              <Nav bsStyle="pills" justified activeKey={this.state.videoId} onSelect={this.handleSelect}>
+                <NavItem className="first" eventKey={"nOQipNj-Nv8"} title="aula 1">Aula 01 (11/09)</NavItem>
+                <NavItem eventKey={2} title="aula 2" disabled><Glyphicon glyph="lock" /> Aula 02 (12/09)</NavItem>
+                <NavItem eventKey={3} title="aula 3" disabled><Glyphicon glyph="lock" /> Aula 03 (13/09)</NavItem>
+                <NavItem eventKey={4} title="aula 4" disabled><Glyphicon glyph="lock" /> Aula 04 (14/09)</NavItem>
+              </Nav>
+            </div>
           </Grid>
           <section className="video">
             <Grid>
