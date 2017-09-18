a7ff55590c4ca887a05a3c4702815d0315afe4af
Nelson Gustavo <nelsongborges@gmail.com>
2017-09-18 11:12:38 -0300

    adding .env

--- src/flux/actions/fire.js
+++ src/flux/actions/fire.js
@@ -1,11 +1,11 @@
 import firebase from 'firebase'
 var config = {
-    apiKey: "***REMOVED***",
-    authDomain: "***REMOVED***",
-    databaseURL: "***REMOVED***",
-    projectId: "***REMOVED***",
-    storageBucket: "***REMOVED***.appspot.com",
-    messagingSenderId: "***REMOVED***"
+    apiKey: process.env.REACT_APP_API_KEY,
+    authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+    databaseURL: process.env.REACT_APP_DATABASE_URL,
+    projectId: process.env.REACT_APP_PROJECT_ID,
+    storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+    messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID
 };
 
 var fire = firebase.initializeApp(config);
